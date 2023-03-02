48eb7122122156933c3284c2805fdac0a44b2c3d
roddydr123 <davidroddy321@gmail.com>
2023-03-02 21:38:24 +0000

    wirte to file

--- game_of_life.py
+++ game_of_life.py
@@ -5,23 +5,28 @@ import sys
 
 
 
-def check_active(grid, i):
+def check_active(grid, i, stop=False):
     active = np.sum(grid)
     with open("active_sites.dat", "a") as f:
         f.write(str(active) + "\n")
-    if i % 10 == 0:
+    if i % 10 == 0 and i != 0:
         with open("active_sites.dat", "r") as f:
             lines = [line.strip() for line in f]
             if len(set(lines[-10:])) <= 1:
-                print(f"stopped after {i} iterations")
-                sys.exit(0)
+                with open("equlilibration_time.dat", "a") as f:
+                    f.write(str(i) + "\n")
+                stop = True
+    return stop
 
 
 
-def update_grid(i, im, grid, grid_size):
+def update_grid(i, im, grid, grid_size, mode):
 
     # count active cells
-    check_active(grid, i)
+    if mode == "R":
+        stop = check_active(grid, i)
+    else:
+        stop = False
 
     # Copy the current grid to avoid overwriting values
     new_grid = grid.copy()
@@ -44,10 +49,17 @@ def update_grid(i, im, grid, grid_size):
                 new_grid[i,j] = 0
             elif grid[i,j] == 0 and num_neighbours == 3:
                 new_grid[i,j] = 1
-    # Update the grid
-    im.set_data(new_grid)
+
     grid[:] = new_grid[:]
-    return im
+
+    if type(im) != str:
+        im.set_data(new_grid)
+        if stop:
+            print(f"stopped after {i} iterations")
+            sys.exit(0)
+        return im
+    else:
+        return stop
 
 
 def random_grid(grid_size):
@@ -75,7 +87,7 @@ def glider_grid(grid_size):
     return grid
 
 
-def visualisation(grid, grid_size, monitor_sites):
+def visualisation(grid, grid_size, mode):
 
     fig, ax = plt.subplots()
 
@@ -83,10 +95,28 @@ def visualisation(grid, grid_size, monitor_sites):
     im = ax.imshow(grid)
 
     # Create the animation object
-    ani = animation.FuncAnimation(fig, update_grid, frames=1000, fargs=(im, grid, grid_size), interval=10)
+    ani = animation.FuncAnimation(fig, update_grid, frames=1000, fargs=(im, grid, grid_size, mode), interval=10)
 
     # Display the animation
     plt.show()
+
+
+def equilibrations():
+
+    im = "equil"
+    grid_size = 50
+    grid = random_grid(grid_size)
+
+    for j in range(10):
+
+        f = open("equlilibration_time.dat", "w")
+        f.close()
+        i = 0
+        stop = False
+        while stop is False:
+            stop = update_grid(i, im, grid, grid_size, "R")
+            i += 1
+
 
 
 def main():
@@ -97,7 +127,6 @@ def main():
     mode = cmd_args[1]
 
     grid_size = 50
-    monitor_sites = True
 
     if mode == "R":
         grid = random_grid(grid_size)
@@ -108,6 +137,7 @@ def main():
     else:
         print("invalid mode")
         sys.exit()
-    visualisation(grid, grid_size, monitor_sites)
+    visualisation(grid, grid_size, mode)
 
-main()
+# main()
+equilibrations()
