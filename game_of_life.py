39f569eb334cc8319d1e7e25955dea96f821a9ec
roddydr123 <davidroddy321@gmail.com>
2023-03-03 15:48:46 +0000

    final game of life

--- game_of_life.py
+++ game_of_life.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import sys
+from scipy.optimize import curve_fit
+
+
+def linear(x, m, c):
+    return (m * x) + c
 
 
 def update_grid(frame, im, grid, grid_size, mode):
@@ -36,7 +41,7 @@ def update_grid(frame, im, grid, grid_size, mode):
         im.set_data(new_grid)
     except:
         pass
-    return im, active_sites
+    return im, active_sites, grid
 
 
 def random_grid(grid_size):
@@ -55,7 +60,7 @@ def oscillator_grid(grid_size):
 
 def glider_grid(grid_size):
     grid = np.zeros((grid_size, grid_size))
-    mid = int(grid_size / 2)
+    mid = int(grid_size / 2.3)
     grid[mid, mid] = 1
     grid[mid + 1, mid] = 1
     grid[mid - 1, mid] = 1
@@ -79,8 +84,6 @@ def visualisation(grid, grid_size, mode):
 
 
 def equilibrations():
-
-    im = "equil"
     grid_size = 50
 
     # number of runs
@@ -95,7 +98,7 @@ def equilibrations():
         while stop is False:
             if i > 10000:
                 stop = True
-            im, active_sites = update_grid(i, None, grid, grid_size, "R")
+            im, active_sites, grid = update_grid(i, None, grid, grid_size, "R")
             sites_list.append(active_sites)
 
             # if unchanged in last 10 iterations
@@ -113,7 +116,77 @@ def plot_equilibrations():
 
     x = list(map(int, lines))
     plt.hist(x, bins=30)
+    plt.xlabel("No. sweeps for equilibration")
+    plt.ylabel("Count")
     print(len(x))
+    plt.show()
+
+
+def c_o_m(grid, grid_size):
+
+    x_list = []
+    y_list = []
+    banned = [0, grid_size]
+    for i in range(grid_size):
+        for j in range(grid_size):
+            if grid[i,j] == 1:
+                if i in banned or j in banned:
+                    return None
+                x_list.append(i)
+                y_list.append(j)
+                
+    com_x = np.average(x_list)
+    com_y = np.average(y_list)
+    return com_x, com_y
+
+
+def plot_glider_com():
+    grid_size = 50
+    grid = glider_grid(grid_size)
+
+    com_x_list = []
+    com_y_list = []
+    times_list = []
+
+    for i in range(200):
+        grid = update_grid(None, None, grid, grid_size, "G")[2]
+        if i % 10 == 0:
+            try:
+                x,y = c_o_m(grid, grid_size)
+                com_x_list.append(x)
+                com_y_list.append(y)
+                times_list.append(i)
+            except:
+                pass
+    times_listv = np.array(times_list)[10:15]
+    com_x_listv = np.array(com_x_list)[10:15]
+    com_y_listv = np.array(com_y_list)[10:15]
+
+    xpopt, xpcov = curve_fit(linear, times_listv, com_x_listv)
+    x_velocity = xpopt[0]
+
+    ypopt, ypcov = curve_fit(linear, times_listv, com_y_listv)
+    y_velocity = ypopt[0]
+
+    total_v = (x_velocity**2 + y_velocity**2)**(1/2)
+
+    print(f"{total_v} pixels per sweep")
+
+    # plt.plot(times_list, linear(times_list, *xpopt), label="Linear fit")
+    # plt.scatter(times_list, com_x_list, label="COM position")
+    # plt.xlabel("Time (sweeps)")
+    # plt.ylabel("X position")
+
+    # plt.plot(times_list, linear(times_list, *ypopt), label="Linear fit")
+    # plt.scatter(times_list, com_y_list, label="COM position")
+    # plt.xlabel("Time (sweeps)")
+    # plt.ylabel("Y position")
+
+    plt.xlabel("X coordinate")
+    plt.ylabel("Y coordinate")
+    plt.title(f"Velocity: {round(total_v, 2)} pixels per sweep")
+    plt.scatter(com_x_list, com_y_list)
+    plt.legend()
     plt.show()
 
 
@@ -139,4 +212,5 @@ def main():
 
 # main()
 # equilibrations()
-plot_equilibrations()
+# plot_equilibrations()
+plot_glider_com()
